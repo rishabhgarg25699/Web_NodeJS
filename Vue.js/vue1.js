c3c471d6a36ebd334e8b2150f9c5ea925eb9093f
Rishabh Garg <rishabhgarg25699@gmail.com>
2019-08-11 17:55:02 +0530

    First Project

--- Vue.js/vue1.js
+++ Vue.js/vue1.js
@@ -1,7 +1,13 @@
 // console.log("Hello World")
 new Vue({
     el: '#app',
+
     data: {
-        title: 'Hello World'
+        title: 'Hello World !!!!'
+    },
+    methods: {
+        changeTitle: function(event) {
+            this.title = event.target.value;
+        }
     }
 })
