6611b85eb3640868d852d0b3d9d24383f2609e88
Rishabh Garg <rishabhgarg25699@gmail.com>
2019-10-22 00:49:20 +0530

    Changed

--- Project Ecommerce/server.js
+++ Project Ecommerce/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 
-server.get('/', function(req, res, next) {
-    res.send("<h1><center> RISHABH GARG </center></h1>");
-})
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+// server.get('/', function(req, res, next) {
+//     res.send("<h1><center> RISHABH GARG </center></h1>");
+// })
 
-server.listen(2121)
+server.use('/', express.static(path.join(__dirname, 'public')));
+server.use('/api', require('./routes/api').route);
+
+server.listen(1111, () => console.log('Server stated at http://localhost:2121'));
