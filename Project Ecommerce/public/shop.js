c1572d29dbd4472df1daf82d49dcaca4f4d772b2
Rishabh Garg <rishabhgarg25699@gmail.com>
2019-10-22 23:41:58 +0530

    Changed

--- Project Ecommerce/public/shop.js
+++ Project Ecommerce/public/shop.js
@@ -16,11 +16,11 @@ function addProduct(name, manuf, price, done) {
 
 function createProductCard(product) {
     return $(`
-    <div class="col-4 card mx-2 p-4">
+    <div class="col-3 row-3 card mx-2 my-2 p-4">
         <h4 class="product-name"> ${product.name}</h4>
         <div class="product-manufacturer">${product.manufacturer}</div>
         <div class="row">
-            <div class="col m-3 p-3">
+            <div class="col row-4 m-3 p-4">
                 <b>Rs. ${product.price}</b>
             </div>
             <button class="col btn btn-primary m-3">Buy</button> 
